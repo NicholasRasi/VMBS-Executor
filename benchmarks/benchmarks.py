ef79a2823d4f23e4cf0370a811a9f6a4661d3084
Nicholas Rasi <nicholas.rasi@gmail.com>
2020-03-10 21:43:05 +0100

    send complete command output

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,5 +1,4 @@
 import os
-import re
 import time
 import statistics
 from benchmarks.wrapper import Wrapper
@@ -48,24 +47,15 @@ class DDBenchmark(Benchmark):
 
     def run(self):
         self.pre()
-
         for bs_count in self.bs_count_list:
-            speeds = []
             bs = bs_count[0]
             count = bs_count[1]
             for r in range(self.repeat):
-                _, output = self.wrapper.dd("/dev/zero", "benchmark", bs, count)
+                retcode, output = self.wrapper.dd("/dev/zero", "benchmark", bs, count)
 
-                regex_speed = r"([0-9,]*) MB\/s"
-                match_speed = re.findall(regex_speed, output, re.MULTILINE)
-
-                if match_speed:
-                    speeds.append(float(str(match_speed[0]).replace(",", ".")))
-            self.result.append(
-                {"setup": {"repeat": self.repeat, "runs": self.repeat, "bs": bs, "count": count},
-                 "result [MB/s]": statistics.mean(speeds)})
+                self.result.append({"setup": {"run": r, "repeat": self.repeat, "bs": bs, "count": count},
+                                    "retcode": retcode, "output": output})
         os.remove('benchmark')
-
         self.post()
         return self.result
 
@@ -84,21 +74,11 @@ class DownloadBenchmark(Benchmark):
         self.url = url
 
     def run(self):
-        size = 0
-        time = 0
-
+        self.pre()
         for r in range(self.repeat):
-            _, output = self.wrapper.curl("/dev/null", self.url)
-
-            match = re.search(r"Downloaded\s+([0-9]+)\sbytes\sin\s([0-9.,]+)\ssec", output)
-            if match:
-                size += round(int(match.group(1)) / 1024 / 1024, 2)  # megabytes
-                try:
-                    time += float(match.group(2))  # sec
-                except ValueError:
-                    time += float(match.group(2).replace(',', '.'))
-        avg_speed = round(size * 8 / time, 2)
-        self.result.append({"setup": {"repeat": self.repeat, "url": self.url}, "result [MB/s]": avg_speed})
+            retcode, output = self.wrapper.curl("/dev/null", self.url)
+            self.result.append({"setup": {"run": r, "repeat": self.repeat, "url": self.url},
+                                "retcode": retcode, "output": output})
         self.post()
         return self.result
 
@@ -122,7 +102,8 @@ class CPUBenchmark(Benchmark):
                     product *= sin(angle) ** 2 + cos(angle) ** 2
             end = time.time()
             durations.append(end - start)
-        self.result.append({"setup": {"repeat": self.repeat}, "result [s]": statistics.mean(durations)})
+            self.result.append({"setup": {"run": r, "repeat": self.repeat},
+                                "result": statistics.mean(durations)})
         self.post()
         return self.result
 
@@ -150,9 +131,10 @@ class AIABenchmark(Benchmark):
                 ai_results = benchmark.run_micro()
             else:
                 ai_results = benchmark.run()
-            self.result.append({"ai_score": ai_results.ai_score,
-                                "inference_score": ai_results.inference_score,
-                                "training_score": ai_results.training_score})
+            self.result.append({"setup": {"run": r, "repeat": self.repeat},
+                                "result": {"ai_score": ai_results.ai_score,
+                                           "inference_score": ai_results.inference_score,
+                                           "training_score": ai_results.training_score}})
         self.post()
         return self.result
 
@@ -164,10 +146,16 @@ class SysBenchmark(Benchmark):
         self.pre()
         sysbench = Sysbench()
         for r in range(self.repeat):
-            self.result.append({"cpu": sysbench.cpu(2000),
-                                "memory": sysbench.memory("1M", "10G"),
-                                "threads": sysbench.threads(10, 128),
-                                "fileio": sysbench.fileio("1GB", "rndrw", 60, 0)})
+            retcode_cpu, output_cpu = sysbench.cpu(2000)
+            retcode_memory, output_memory = sysbench.memory("1M", "10G")
+            retcode_threads, output_threads = sysbench.threads(10, 128)
+            retcode_fileio, output_fileio = sysbench.fileio("1GB", "rndrw", 60, 0)
+
+            self.result.append({"setup": {"run": r, "repeat": self.repeat},
+                                "result": {"cpu": {"retcode": retcode_cpu, "output": output_cpu},
+                                           "memory": {"retcode": retcode_memory, "output": output_memory},
+                                           "threads": {"retcode": retcode_threads, "output": output_threads},
+                                           "fileio": {"retcode": retcode_fileio, "output": output_fileio}}})
 
         self.post()
         return self.result
@@ -186,17 +174,11 @@ class WebServerBenchmark(Benchmark):
             time.sleep(5)
             self.logger.info("Starting load generator...")
             # run load generator
-            _, output = self.wrapper.wrk(12, 24, 30, "http://localhost:8000")
+            retcode, output = self.wrapper.wrk(12, 24, 30, "http://localhost:8000")
             web_server.terminate()
             self.logger.info(output)
 
-            regex_req_s = r"Requests/sec:\s*([0-9.]*)"
-            match_req_s = re.findall(regex_req_s, output, re.MULTILINE)
-
-            req_s = None
-            if match_req_s:
-                req_s = match_req_s[0]
-            self.result.append({"result [req/s]": req_s})
-
+            self.result.append({"setup": {"run": r, "repeat": self.repeat},
+                                "result": {"retcode": retcode, "output": output}})
         self.post()
         return self.result
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 import time
 import logging
 import coloredlogs
+import argparse
+import requests
 from benchmarks.benchmarks import DDBenchmark, DownloadBenchmark, CPUBenchmark, AIABenchmark, SysBenchmark, WebServerBenchmark
 from benchmarks.server import Server
 
@@ -9,6 +11,11 @@ coloredlogs.install(level='INFO', milliseconds=False)
 
 logger = logging.getLogger("cloud-benchmark")
 logger.info("Start cloud benchmark")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bin', type=str, required=True)
+args = parser.parse_args()
+
 
 benchmark_results = []
 benchmarks = []
@@ -21,19 +28,19 @@ dd_benchmark.set_bs_count([("512MB", 1), ("512", 1000)])
 benchmarks.append(dd_benchmark)
 # download benchmark
 download_benchmark = DownloadBenchmark(repeat=1, logger=logger)
-benchmarks.append(download_benchmark)
+#benchmarks.append(download_benchmark)
 # simple CPU
 cpu_benchmark = CPUBenchmark(repeat=100, logger=logger)
 benchmarks.append(cpu_benchmark)
 # ai benchmark
 ai_benchmark = AIABenchmark(repeat=1, logger=logger, type="micro")
-benchmarks.append(ai_benchmark)
+#benchmarks.append(ai_benchmark)
 # sysbenchmark
 sys_benchmark = SysBenchmark(repeat=1, logger=logger)
 benchmarks.append(sys_benchmark)
 # web server benchmark
 web_benchmark = WebServerBenchmark(repeat=1, logger=logger)
-benchmarks.append(web_benchmark)
+#benchmarks.append(web_benchmark)
 
 for benchmark in benchmarks:
     benchmark_results.append({"name": benchmark.name, "results": benchmark.run()})
@@ -41,4 +48,7 @@ for benchmark in benchmarks:
 end = time.time()
 
 benchmark_result = {"duration": end - start, "server": Server.get_all(), "benchmarks": benchmark_results}
-logging.info("Sending payload: " + json.dumps(benchmark_result))
+logging.info("Sending payload: " + json.dumps(benchmark_result) + " to " + args.bin)
+
+response = requests.post(args.bin, benchmark_result)
+logging.info(response)
