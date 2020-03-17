82d67e939e7d9108405ccaa1730dbdfb2a903ee8
Nicholas Rasi <nicholas.rasi@gmail.com>
2020-03-17 12:04:29 +0100

    complete refactoring

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -10,14 +10,19 @@ from .python_sysbench import Sysbench
 class Benchmark:
     name = None
 
-    def __init__(self, repeat, logger):
-        self.repeat = repeat
+    def __init__(self, logger):
         self.logger = logger
         self.wrapper = Wrapper(self.logger)
-        self.result = []
+        self.result = None
 
     def pre(self):
         self.logger.info("Starting " + self.name + " benchmark...")
+
+    def setup(self, setup):
+        self.setup = setup
+
+    def get_setup(self):
+        return self.setup
 
     def run(self):
         """Abstract method, must be overridden in child class."""
@@ -34,28 +39,17 @@ class DDBenchmark(Benchmark):
     """
     name = 'dd'
 
-    def __init__(self, repeat, logger):
-        super().__init__(repeat, logger)
-        self.bs_count_list = None
-
-    def set_bs_count(self, bs_count_list):
-        self.bs_count_list = bs_count_list
-
     def pre(self):
-        self.logger.info(
-            "Starting " + self.name + " benchmark with bs_count=" + str(self.bs_count_list))
+        self.logger.info("Starting " + self.name + " benchmark with bs=" + str(self.setup["bs"]) + ", count=" +
+                         str(self.setup["count"]))
 
     def run(self):
         self.pre()
-        for bs_count in self.bs_count_list:
-            bs = bs_count[0]
-            count = bs_count[1]
-            for r in range(self.repeat):
-                retcode, output = self.wrapper.dd("/dev/zero", "benchmark", bs, count)
 
-                self.result.append({"setup": {"run": r, "repeat": self.repeat, "bs": bs, "count": count},
-                                    "retcode": retcode, "output": output})
+        retcode, output = self.wrapper.dd("/dev/zero", "benchmark", self.setup["bs"], self.setup["count"])
+        self.result = {"retcode": retcode, "output": output}
         os.remove('benchmark')
+
         self.post()
         return self.result
 
@@ -66,19 +60,12 @@ class DownloadBenchmark(Benchmark):
     """
     name = 'download'
 
-    def __init__(self, repeat, logger):
-        super().__init__(repeat, logger)
-        self.url = 'http://cachefly.cachefly.net/100mb.test'
-
-    def set_url(self, url):
-        self.url = url
-
     def run(self):
         self.pre()
-        for r in range(self.repeat):
-            retcode, output = self.wrapper.curl("/dev/null", self.url)
-            self.result.append({"setup": {"run": r, "repeat": self.repeat, "url": self.url},
-                                "retcode": retcode, "output": output})
+
+        retcode, output = self.wrapper.curl("/dev/null", self.setup["url"])
+        self.result = {"retcode": retcode, "output": output}
+
         self.post()
         return self.result
 
@@ -92,18 +79,18 @@ class CPUBenchmark(Benchmark):
 
     def run(self):
         self.pre()
+
         durations = []
-        for r in range(self.repeat):
-            start = time.time()
-            product = 1.0
-            for counter in range(1, 1000, 1):
-                for dex in list(range(1, 360, 1)):
-                    angle = radians(dex)
-                    product *= sin(angle) ** 2 + cos(angle) ** 2
-            end = time.time()
-            durations.append(end - start)
-        self.result.append({"setup": {"repeat": self.repeat},
-                            "result": statistics.mean(durations)})
+        start = time.time()
+        product = 1.0
+        for counter in range(1, 1000, 1):
+            for dex in list(range(1, 360, 1)):
+                angle = radians(dex)
+                product *= sin(angle) ** 2 + cos(angle) ** 2
+        end = time.time()
+        durations.append(end - start)
+        self.result = statistics.mean(durations)
+
         self.post()
         return self.result
 
@@ -115,26 +102,22 @@ class AIABenchmark(Benchmark):
     """
     name = 'ai-benchmark'
 
-    def __init__(self, repeat, logger, type=None):
-        super().__init__(repeat, logger)
-        self.type = type
-
     def run(self):
         self.pre()
+
         benchmark = AIBenchmark()
-        for r in range(self.repeat):
-            if self.type == "inference":
-                ai_results = benchmark.run_inference()
-            elif self.type == "training":
-                ai_results = benchmark.run_training()
-            elif self.type == "micro":
-                ai_results = benchmark.run_micro()
-            else:
-                ai_results = benchmark.run()
-            self.result.append({"setup": {"run": r, "repeat": self.repeat},
-                                "result": {"ai_score": ai_results.ai_score,
-                                           "inference_score": ai_results.inference_score,
-                                           "training_score": ai_results.training_score}})
+        if self.setup["type"] == "inference":
+            ai_results = benchmark.run_inference()
+        elif self.setup["type"] == "training":
+            ai_results = benchmark.run_training()
+        elif self.setup["type"] == "micro":
+            ai_results = benchmark.run_micro()
+        else:
+            ai_results = benchmark.run()
+        self.result = {"ai_score": ai_results.ai_score,
+                       "inference_score": ai_results.inference_score,
+                       "training_score": ai_results.training_score}
+
         self.post()
         return self.result
 
@@ -145,18 +128,17 @@ class SysBenchmark(Benchmark):
     def run(self):
         self.pre()
         sysbench = Sysbench(return_full_output=True)
-        for r in range(self.repeat):
-            output_cpu = sysbench.cpu(2000)
-            output_memory = sysbench.memory("1M", "10G")
-            output_threads = sysbench.threads(10, 128)
-            output_fileio = sysbench.fileio("1GB", "rndrw", 60, 0)
 
-            self.result.append({"setup": {"run": r, "repeat": self.repeat},
-                                "result": {"cpu": {"output": output_cpu},
-                                           "memory": {"output": output_memory},
-                                           "threads": {"output": output_threads},
-                                           "fileio": {"output": output_fileio}}})
+        output_cpu = sysbench.cpu(self.setup["cpu_max_prime"])
+        output_memory = sysbench.memory(self.setup["mem_block_size"], self.setup["mem_total_size"])
+        output_threads = sysbench.threads(self.setup["threads_max_time"], self.setup["threads_num"])
+        output_fileio = sysbench.fileio(self.setup["file_total_size"], self.setup["file_test_mode"],
+                                        self.setup["file_max_time"], self.setup["file_max_requests"])
 
+        self.result = {"cpu": {"output": output_cpu},
+                       "memory": {"output": output_memory},
+                       "threads": {"output": output_threads},
+                       "fileio": {"output": output_fileio}}
         self.post()
         return self.result
 
@@ -170,19 +152,18 @@ class WebServerBenchmark(Benchmark):
     def run(self):
         self.pre()
 
-        for r in range(self.repeat):
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            web_server = self.wrapper.gunicorn(12, dir_path + "/webserver/", "main:create_app()", bg=True)
-            self.logger.info("Web server started...")
-            time.sleep(5)
-            self.logger.info("Starting load generator...")
-            # run load generator
-            retcode, output = self.wrapper.wrk(12, 24, 30, "http://localhost:8000")
-            web_server.terminate()
-            self.logger.info(output)
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        web_server = self.wrapper.gunicorn(12, dir_path + "/webserver/", "main:create_app()", bg=True)
+        self.logger.info("Web server started...")
+        time.sleep(5)
+        self.logger.info("Starting load generator...")
+        # run load generator
+        retcode, output = self.wrapper.wrk(self.setup["threads"], self.setup["connections"], self.setup["time"],
+                                           "http://localhost:8000")
+        web_server.terminate()
+        self.logger.info(output)
+        self.result = {"retcode": retcode, "output": output}
 
-            self.result.append({"setup": {"run": r, "repeat": self.repeat},
-                                "result": {"retcode": retcode, "output": output}})
         self.post()
         return self.result
 
@@ -196,11 +177,15 @@ class NenchBenchmark(Benchmark):
     def run(self):
         self.pre()
 
-        for r in range(self.repeat):
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            retcode, output = self.wrapper.bash(dir_path + "/nench/nench.sh")
-            self.result.append({"setup": {"run": r, "repeat": self.repeat},
-                                "result": {"retcode": retcode, "output": output}})
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        retcode, output = self.wrapper.bash(dir_path + "/nench/nench.sh")
+        self.result = {"retcode": retcode, "output": output}
 
         self.post()
         return self.result
+
+def get_benchmark_class(name):
+    return list(filter(lambda bench: bench.name == name, BENCHMARKS))[0]
+
+BENCHMARKS = [DDBenchmark, DownloadBenchmark, CPUBenchmark, AIABenchmark, SysBenchmark, WebServerBenchmark,
+              NenchBenchmark]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import random
 import time
 import logging
 import coloredlogs
@@ -6,71 +7,69 @@ import argparse
 import requests
 import yaml
 import datetime
-from benchmarks.benchmarks import DDBenchmark, DownloadBenchmark, CPUBenchmark, AIABenchmark, SysBenchmark, \
-    WebServerBenchmark, NenchBenchmark
+from benchmarks.benchmarks import get_benchmark_class
 from benchmarks.server import Server
 
+# setup logger
 coloredlogs.install(level='INFO', milliseconds=False)
-
 logger = logging.getLogger("cloud-benchmark")
 logger.info("Start cloud benchmark")
 
+# parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--bin', type=str, required=False)
 args = parser.parse_args()
 
+# read bin id from file if not provided as an argument
 if args.bin:
     bin_id = args.bin
 else:
     f = open("bin_id.txt", "r")
     bin_id = (f.read())
 
+# read configuration file
 with open("config.yml", 'r') as file:
     data = file.read()
     config = yaml.load(data, Loader=yaml.FullLoader)
 
+# setup bin url
 sending_bin = config["bin_database_url"] + "/" + str(bin_id).strip()
 logging.info("Sendind data to: " + sending_bin)
 
 benchmark_results = []
-benchmarks = []
-start = time.time()
+benchmarks_list = []
+
+benchmarks = config["benchmarks"]
+logger.info("Running benchmarks: " + ", ".join(benchmarks.keys()))
 
 # set up benchmarks
-# dd benchmark
-dd_benchmark = DDBenchmark(repeat=2, logger=logger)
-dd_benchmark.set_bs_count([("512MB", 1), ("512", 1000)])
-#benchmarks.append(dd_benchmark)
-# download benchmark
-download_benchmark = DownloadBenchmark(repeat=1, logger=logger)
-#benchmarks.append(download_benchmark)
-# simple CPU
-cpu_benchmark = CPUBenchmark(repeat=1, logger=logger)
-#benchmarks.append(cpu_benchmark)
-# ai benchmark
-ai_benchmark = AIABenchmark(repeat=1, logger=logger, type="micro")
-#benchmarks.append(ai_benchmark)
-# sysbenchmark
-sys_benchmark = SysBenchmark(repeat=1, logger=logger)
-#benchmarks.append(sys_benchmark)
-# web server benchmark
-web_benchmark = WebServerBenchmark(repeat=1, logger=logger)
-#benchmarks.append(web_benchmark)
-# nench benchmark
-nench_benchmark = NenchBenchmark(repeat=1, logger=logger)
-#benchmarks.append(nench_benchmark)
+for benchmark_name in benchmarks.keys():
+    benchmark_class = get_benchmark_class(benchmark_name)
+    logger.info("Adding " + benchmark_name + " with: " + str(benchmarks[benchmark_name]["setup"]))
+    for repeat in range(benchmarks[benchmark_name]["repeat"]):
+        for setup in benchmarks[benchmark_name]["setup"]:
+            benchmark = benchmark_class(logger=logger)
+            benchmark.setup(setup)
+            benchmarks_list.append(benchmark)
 
-for benchmark in benchmarks:
-    benchmark_results.append({"name": benchmark.name, "results": benchmark.run()})
+# randomize benchmark list
+random.shuffle(benchmarks_list)
+logger.info("Benchmark list: " + ", ".join([benchmark.name for benchmark in benchmarks_list]))
 
+# start running benchmarks
+start = time.time()
+for benchmark in benchmarks_list:
+    benchmark_results.append({"name": benchmark.name, "setup": benchmark.get_setup(), "results": benchmark.run()})
 end = time.time()
 
+# build payload
 benchmark_result = {"time": str(datetime.datetime.now()),
                     "duration": end - start,
                     "server": Server.get_all(),
                     "benchmarks": benchmark_results}
-
 payload = json.dumps(benchmark_result)
 logging.info("Sending payload: " + payload + " to " + sending_bin)
+
+# send data
 response = requests.post(sending_bin, payload, headers={'content-type': 'application/json'})
-logging.info(response)
+logging.info(response.text)
